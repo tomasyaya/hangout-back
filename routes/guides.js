c0ace40c96ba0fdf0516a783f0c0df92e166d30f
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-05-29 10:33:33 +0200

    create guide route working

--- routes/guides.js
+++ routes/guides.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const guideControllers = require('../controllers/guideControllers');
+const { isLoggedIn } = require('../helpers/middlewares');
 
 router.get('/', (req, res, next) => {
   console.log('hit the route')
 })
 
-router.post('/', guideControllers.createGuide)
+router.post('/', isLoggedIn(), guideControllers.createGuide)
 
 module.exports = router;
